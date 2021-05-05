9ddba346f203026a7c8fe5ade60aa5f15104637c
DenPixel <dev.den.bond@gmail.com>
2021-05-05 19:25:37 +0300

    Start

---
+++ src/main/java/com/denpixel/jeracshop/JeracshopApplication.java
@@ -0,0 +1,13 @@
+package com.denpixel.jeracshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeracshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeracshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/denpixel/jeracshop/JeracshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denpixel.jeracshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeracshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
